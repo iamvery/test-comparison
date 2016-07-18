ed7fcbe8da7d3efadb3f80e984fdd619c10a0a6e
Jay Hayes <ur@iamvery.com>
2016-07-18 12:52:50 -0500

    add js and ruby tests

---
+++ spec/speed_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe "speed" do
+  it "is fast" do
+    expect(true).to eq(true)
+  end
+end
---
+++ test/test.js
@@ -0,0 +1,7 @@
+var {expect} = require('chai');
+
+describe('speed', () => {
+  it('is fast', () => {
+    expect(true).to.equal(true);
+  });
+});
